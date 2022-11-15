8f740c44ccda4e8fe69da56949ca28e62f9ae54a
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-11-15 22:11:01 +0600

    tailwind configuration practice

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,21 @@ module.exports = {
   darkMode: "media",
   content: ["*.html"],
   theme: {
+    // screens: {
+    //   sm: "500px",
+    //   md: "768px",
+    //   lg: "992px",
+    //   xl: "1280px",
+    //   "2xl": "1536px",
+    // },
     extend: {
-      colors: {
-        gray: {
-          100: "red",
-          200: "blue",
-          1000: "orange",
-        },
-      },
+      // colors: {
+      //   gray: {
+      //     100: "red",
+      //     200: "blue",
+      //     1000: "orange",
+      //   },
+      // },
     },
   },
   plugins: [],
